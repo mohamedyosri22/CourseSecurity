ae236f6b4679b1990bfea0b0d87b78daaec56252
mhmdyouali <mhmd.you@yahoo.com>
2022-12-31 16:11:08 +0200

    project init

---
+++ src/main/java/com/spring/CourseSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSecurityApplication.class, args);
+    }
+
+}
